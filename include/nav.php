a03349f34b3fa240c8ad413da78f0c43e4ff0928
Toshendra Sharma <me@toshendra.com>
2016-12-28 22:14:55 +0530

    nav settings updated

--- include/nav.php
+++ include/nav.php
@@ -11,7 +11,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/poe/">Proof-of-Existence</a>
+                <a class="navbar-brand" href="/">Proof-of-Existence</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
